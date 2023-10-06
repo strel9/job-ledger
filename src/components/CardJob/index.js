fa855d87beece13e2a69c736ed39e389b73cfd65
strel9 <strel9@gmail.com>
2023-10-06 19:06:16 +0300

    fixed cardJob

--- src/components/CardJob/index.js
+++ src/components/CardJob/index.js
@@ -73,7 +73,7 @@ export default function CardJob (props) {
             variant='h4'
             sx={{ mb: 0.8 }}
           >
-            {id} {jobTitle}
+            {jobTitle}
           </Typography>
           <div>
             <Typography
