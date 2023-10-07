263d45f7f68b05cb7658fea82410df94bede0eb4
strel9 <strel9@gmail.com>
2023-10-07 20:36:14 +0300

    added google analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document () {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <Script
+          strategy='afterInteractive'
+          src='https://www.googletagmanager.com/gtag/js?id=G-3ZCXN2P33Y'
+        />
+        <Script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3ZCXN2P33Y');
+        `}
+        </Script>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
