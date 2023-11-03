6941e22aef8090871a82707aeb928060616412ac
strel9 <strel9@gmail.com>
2023-11-03 20:01:53 +0200

    fixed Crisp

--- src/components/Crisp.js
+++ src/components/Crisp.js
@@ -5,7 +5,7 @@ import { Crisp } from 'crisp-sdk-web'
 
 const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure('58b1471e-41')
+    Crisp.configure('58b1471e-413e-4766-b017-4e1088c6beac')
   })
 
   return null
